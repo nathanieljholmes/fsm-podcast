89e8d3036bc222de3d94faf5b3508dec3eb338a9
nathanieljholmes <nathanielholmesdevelopment@gmail.com>
2021-02-24 17:12:49 -0700

    CHNGE Added Form.html and IndexAlt.js

---
+++ index.js
@@ -0,0 +1,17 @@
+
+    
+const submitButton = document.getElementById("submitBtn");
+const email = document.getElementById("email");
+const error = document.getElementById("errorEvt");
+error.style.display = "none"; 
+
+submitButton.addEventListener("click", (ev) => {
+    console.log(email.value)
+    ev.preventDefault();
+    const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(mailFormat.test(email.value)){
+        error.style.display = "none"; 
+    } else {
+        error.style.display = "block";
+    }
+});
